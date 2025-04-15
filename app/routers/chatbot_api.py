bb6d863801e7c24087ebe103f40badc76280c8f7
haihai <truongvanhai868@gmail.com>
2025-04-15 22:59:00 +0700

    add auth to chatbot

--- app/routers/chatbot_api.py
+++ app/routers/chatbot_api.py
@@ -1,5 +1,5 @@
-from fastapi import APIRouter
-from app import schemas
+from fastapi import APIRouter, Depends
+from app import schemas, models, security
 
 from chatbot.rag_src.main_chain import answer_question
 
@@ -9,6 +9,6 @@ router = APIRouter(
 )
 
 @router.post('/ask', response_model=schemas.ChatbotResponse)
-def ask_question(request: schemas.ChatbotRequest):
+def ask_question(request: schemas.ChatbotRequest, user: models.User = Depends(security.get_current_user)):
     answer, total_time, retrieval_time, llm_time = answer_question(request.question)
     return {'answer': answer, 'total_time': total_time, 'retrieval_time': retrieval_time, 'llm_time': llm_time}

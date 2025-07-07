8c19fe4b9bd90551fdf34eab1ad81c658c6bdada
haihai <truongvanhai868@gmail.com>
2025-07-07 15:57:02 +0700

    add tools

--- app/main.py
+++ app/main.py
@@ -10,7 +10,7 @@ models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
-origins = ['http:/localhost:3000']
+origins = ['http://localhost:3000']
 
 app.add_middleware(
     CORSMiddleware,
--- chatbot/draw_graph.py
+++ chatbot/draw_graph.py
@@ -1,5 +1,3 @@
-from langchain_core.runnables.graph import MermaidDrawMethod
-
 from chatbot.real_main_agent import graph
 
 image_data = graph.get_graph().draw_mermaid_png()
